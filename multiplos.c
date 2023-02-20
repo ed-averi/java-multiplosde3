338ac079d1674bffeefdd827253c1971dd62642d
Gerson Vasquez <62489511+ed-averi@users.noreply.github.com>
2023-02-20 13:31:37 -0600

    Ejemplo en C#

---
+++ multiplos.c
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+  public static void Main()
+  {
+    for(int numero=0; numero<=100; numero++){
+      if(numero % 3== 0){
+        Console.WriteLine(numero);
+      }
+    }
+  }
+
+}
